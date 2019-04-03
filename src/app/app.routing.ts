e74a94ea68d6836c15a0843c0b65568537149da9
Student <example@example.com>
2019-04-03 16:42:23 -0700

    add firebase, more to come

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -5,6 +5,7 @@ import { WelcomeComponent } from './welcome/welcome.component';
 import { AboutComponent } from './about/about.component';
 import { MarketplaceComponent } from './marketplace/marketplace.component';
 import { AlbumDetailComponent }   from './album-detail/album-detail.component';
+import { AdminComponent }   from './admin/admin.component';
 
 
 //array of routs
@@ -24,6 +25,10 @@ const appRoutes: Routes = [
   {
     path: 'albums/:id',
     component: AlbumDetailComponent
+  },
+  {
+    path: 'admin',
+    component: AdminComponent
   }
 ];
 
